754f2de2090331053fa157a5a11e2433083fa61a
tongshengzhi <tsz_email@163.com>
2020-08-27 21:17:45 +0800

    init

---
+++ index-compiled.js
@@ -0,0 +1,47 @@
+const React = require('react');
+
+const {
+  renderToString
+} = require('react-dom/server');
+
+const http = require('http'); // import Index from './test.js';
+//组件
+
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handlerClick() {
+    alert('一起来玩 react ssr 啊');
+  }
+
+  render() {
+    return /*#__PURE__*/React.createElement("h1", {
+      onClick: this.handlerClick
+    }, "click here!");
+  }
+
+} //服务
+
+
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  const html = renderToString( /*#__PURE__*/React.createElement(Index, null));
+  res.end(`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>React SSR</title>
+		</head>
+		<body>
+		    <div id="root">
+		       ${html}
+		    </div>
+		</body>
+		</html>`);
+}).listen(9001);
+console.log('server start. 9001');
---
+++ server/test.js
@@ -0,0 +1,12 @@
+const React = require('react');
+
+//组件
+export default class Index extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>hello react ssr!</h1>
+    }
+}
