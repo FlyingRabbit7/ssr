3f0684df3ab1f9130e7814a24678f3907cfde901
tongshengzhi <tsz_email@163.com>
2020-08-27 21:30:37 +0800

    安装包

--- index-compiled.js
+++ index-compiled.js
@@ -1,47 +1,70 @@
-const React = require('react');
+"use strict";
 
-const {
-  renderToString
-} = require('react-dom/server');
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-const http = require('http'); // import Index from './test.js';
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var React = require('react');
+
+var _require = require('react-dom/server'),
+    renderToString = _require.renderToString;
+
+var http = require('http'); // import Index from './test.js';
 //组件
 
 
-class Index extends React.Component {
-  constructor(props) {
-    super(props);
+var Index = /*#__PURE__*/function (_React$Component) {
+  _inherits(Index, _React$Component);
+
+  var _super = _createSuper(Index);
+
+  function Index(props) {
+    _classCallCheck(this, Index);
+
+    return _super.call(this, props);
   }
 
-  handlerClick() {
-    alert('一起来玩 react ssr 啊');
-  }
+  _createClass(Index, [{
+    key: "handlerClick",
+    value: function handlerClick() {
+      alert('一起来玩 react ssr 啊');
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("h1", {
+        onClick: this.handlerClick
+      }, "click here!");
+    }
+  }]);
 
-  render() {
-    return /*#__PURE__*/React.createElement("h1", {
-      onClick: this.handlerClick
-    }, "click here!");
-  }
-
-} //服务
+  return Index;
+}(React.Component); //服务
 
 
-http.createServer((req, res) => {
+http.createServer(function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  const html = renderToString( /*#__PURE__*/React.createElement(Index, null));
-  res.end(`<!DOCTYPE html>
-		<html lang="en">
-		<head>
-		    <meta charset="UTF-8">
-		    <title>React SSR</title>
-		</head>
-		<body>
-		    <div id="root">
-		       ${html}
-		    </div>
-		</body>
-		</html>`);
+  var html = renderToString( /*#__PURE__*/React.createElement(Index, null));
+  res.end("<!DOCTYPE html>\n\t\t<html lang=\"en\">\n\t\t<head>\n\t\t    <meta charset=\"UTF-8\">\n\t\t    <title>React SSR</title>\n\t\t</head>\n\t\t<body>\n\t\t    <div id=\"root\">\n\t\t       ".concat(html, "\n\t\t    </div>\n\t\t</body>\n\t\t</html>"));
 }).listen(9001);
 console.log('server start. 9001');
